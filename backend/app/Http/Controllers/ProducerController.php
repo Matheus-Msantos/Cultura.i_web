4fb0c4c9cf3b9a333a95cc581eea5254b3c6f75c
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-03-27 19:25:39 -0300

    Format rows

--- backend/app/Http/Controllers/ProducerController.php
+++ backend/app/Http/Controllers/ProducerController.php
@@ -6,40 +6,40 @@ use App\Models\Producer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class ProducerController extends Controller
-{
+class ProducerController extends Controller {
     public function indexApi() {
-        return response()->json(Producer::all());
+        return response()->json( Producer::all() );
     }
 
-    public function storeApi(Request $request) {
+    public function storeApi( Request $request ) {
 
-if($request->logo) {
-    $logo = $request->file('logo')->store('/public/producer');
-    $logo = str_replace('public/', 'storage/', $logo);
-}else {
-    $logo = "storage/logoDefault.png";
-}
+        if ( $request->logo ) {
+            $logo = $request->file( 'logo' )->store( '/public/producer' );
+            $logo = str_replace( 'public/', 'storage/', $logo );
+        } else {
+            $logo = 'storage/logoDefault.png';
+        }
 
-$producer = Producer::create([
-    'name' => $request->name,
-    'email' => $request->email,
-    'cnpj' => $request->cnpj,
-    'password' => Hash::make($request->password),
-    'media' => $request->media,
-    'logo' => $logo
-]);
+        $producer = Producer::create( [
+            'name' => $request->name,
+            'email' => $request->email,
+            'cnpj' => $request->cnpj,
+            'password' => Hash::make( $request->password ),
+            'media' => $request->media,
+            'logo' => $logo
+        ] );
 
-        /*$producer = Producer::create($request->all());*/
-        return response()->json($producer);
+        /*$producer = Producer::create( $request->all() );
+        */
+        return response()->json( $producer );
     }
 
-    public function showApi(Producer $producer) {
-        return response()->json($producer);
+    public function showApi( Producer $producer ) {
+        return response()->json( $producer );
     }
 
-    public function updateApi(Request $request, Producer $producer) {
-        $producer->update($request->all());
-        return response()->json($producer);
+    public function updateApi( Request $request, Producer $producer ) {
+        $producer->update( $request->all() );
+        return response()->json( $producer );
     }
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -7,25 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\OrderItem;
 use App\Models\Cart;
 
-class Product extends Model
-{
+class Product extends Model {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'time', 'date', 'classification', 'category_id', 'price', 'address_id', 'image'];
+    protected $fillable = [ 'name', 'description', 'time', 'date', 'classification', 'category_id', 'price', 'address_id', 'image' ];
 
     public function category() {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo( Category::class );
     }
 
     public function address() {
-        return $this->belongsTo(Address::class);
+        return $this->belongsTo( Address::class );
     }
 
     public function orderItem() {
-        return $this->hasMany(OrderItem::class);
+        return $this->hasMany( OrderItem::class );
     }
 
     public function cart() {
-        return $this->hasMany(Cart::class);
+        return $this->hasMany( Cart::class );
     }
 }
