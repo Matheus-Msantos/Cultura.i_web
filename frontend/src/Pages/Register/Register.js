35863cdb4c9a4b58b7a7bf1603404594dabf8cd8
Bruno Pontes <bruno_confessor@hotmail.com>
2022-04-30 00:37:15 -0300

    Iniciado criacao de pagina de registrar-se

--- frontend/src/Pages/Register/Register.js
+++ frontend/src/Pages/Register/Register.js
@@ -1,9 +1,35 @@
 import React from "react";
+import './Register.scss';
+import imgLogin from "../../image/img-login-user.png";
 
-function RegisterPage() {
+function LoginPage() {
     return (
-        <h1>Register</h1>
+        <div className="div-register">
+            <div className="div-img-email-pass">
+                <div className="div-img">
+                    <img className="img" src={imgLogin} />
+                </div>
+                <form className="form-input-email-pass-info" method="POST" action="#">
+                    <div className="div-cadastro">
+                        <h1 className="h1-cadastro" >CADASTRO</h1>
+                    </div>
+                    <div className="div-input-email-pass">
+                        <div className="div-email">
+                            <span className="span-email">Email</span>
+                            <input className="input-email" type="email" />
+                        </div>
+                        <div className="div-pass">
+                            <span className="span-pass">Senha</span>
+                            <input className="input-pass" type="password" />
+                        </div>
+                    </div>
+                    <div className="div-register-button">
+                        <button className="btn-register">CADASTRAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
-export default RegisterPage;
+export default LoginPage;
