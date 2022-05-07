e9f2ec918a42416f890610e79182476e66fd0543
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-05-07 09:45:03 -0300

    footer criado

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -6,9 +6,13 @@ import { UserArray } from "../../Auth";
 import "./Layout.scss";
 
 function Layout({ children }) {
+
+    /* Constante global dos dados do usuário */
     const userAuth = useContext(UserArray);
+    /* Constante */
     const [box, setBox] = useState(false)
 
+    /* Função para abrir ou fechar o popup do usuário */
     const handleOpenBox = () => {
         if (box === false)
             setBox(true);
@@ -32,19 +36,15 @@ function Layout({ children }) {
                     <div className="layout-header-login_container">
 
                         <div className="layout-header-login-user_container">
-                            {!!userAuth ?
+
+                            {!!userAuth ? /* Fazendo verificação se o usuário estiver logado, mostar o nome se não mostrar o botão de login  */
                                 <button className="layout-header-login-user_text" onClick={handleOpenBox}>
                                     <i className="fa-solid fa-circle-user"></i>Olá, {userAuth[0].user.name}
                                 </button>
-
                                 :
-
                                 <Link to="/login" className="layout-header-login-user_button">Entrar <i className="fa-solid fa-right-to-bracket"></i></Link>
                             }
-
-
                             <LoginBox box={box} />
-
                         </div>
 
                         <button className="layout-header-login_carrinho">Carrinho</button>
@@ -57,8 +57,7 @@ function Layout({ children }) {
             </section>
 
             <footer className="layout-footer_container">
-                [Footer]
-                Esperando Layouts
+                <p>Cultura.í&reg;  2022</p>
             </footer>
         </>
     );
