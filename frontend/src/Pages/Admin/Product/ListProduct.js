d2620ee519f7a34e2ec6e17b25781efb171f27c8
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-05-10 22:50:00 -0300

    Página de lista ,Registro de eventos e Atualização de eventos

--- frontend/src/Pages/Admin/Product/ListProduct.js
+++ frontend/src/Pages/Admin/Product/ListProduct.js
@@ -1,12 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import MenuAdmin from "../../../components/MenuAdmin";
+import { BaseUrl } from "../../../Api/baseUrl";
 
 import '../Admin.scss';
+import OptionBoxAdmin from "../../../components/OptionBoxAdmin/index";
 
 function AdminProductPage() {
+
+    const [produtos, setProdutos] = useState([]);
+    const [box, setBox] = useState(false);
+
+    useEffect(() => {
+        /* Chamada da API de todos os produtos */
+        BaseUrl
+            .get("/api/product")
+            .then((res) => setProdutos(res.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+
+    const handleBox = () => {
+        if (box === false)
+            setBox(true);
+        else
+            setBox(false);
+    }
+
+    /* Mapeando todos os produtos e adicionando na página */
+    const MapProdutos = produtos.map((produto) => {
+        const { id, image, name, date, time, address, category, price, classification } = produto;
+        return (
+            <tr key={id}>
+                <th scope="row">{id}</th>
+                <td>{name}</td>
+                <td>{date}</td>
+                <td>{time}</td>
+                <td>R$ {price}</td>
+                <td>{category?.name}</td>
+                <td>{address?.street}</td>
+                <td>{classification}</td>
+                <td>
+                    <img src={image} />
+                </td>
+                <td>
+                    <button onClick={() => handleBox()}><i className="fa-solid fa-ellipsis"></i></button>
+
+                    <div className={`admin-box_container ${box && 'is--active'}`}>
+                        <OptionBoxAdmin id={id} />
+                    </div>
+                </td>
+            </tr>
+        );
+    });
+
     return (
         <div className="admin-container">
             <MenuAdmin active_01={"is--active"} />
+
+            <div className="admin-content_container">
+                <Link to="/admin/product/register" className="admin-content-button_add"><i className="fa-solid fa-plus"></i>Adicionar</Link>
+
+                <h2>Eventos</h2>
+
+                <table className="table align-middle">
+                    <thead >
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Preço</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Classificação</th>
+                            <th scope="col">Imagem</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        {MapProdutos}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
--- frontend/src/Pages/Admin/Product/RegisterProduct.js
+++ frontend/src/Pages/Admin/Product/RegisterProduct.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import MenuAdmin from "../../../components/MenuAdmin";
 
 function AdminRegisterProductPage() {
     return (
-        <h1>Admin Product register</h1>
+        <div className="admin-container">
+            <MenuAdmin active_01={"is--active"} />
+
+            <div className="admin-content_container">
+                <Link to="/admin" className="admin-content-button_add"><i className="fa-solid fa-left"></i> Voltar</Link>
+
+                <h2>Cadastrar Evento</h2>
+
+                <form>
+                    <input type="text" className="admin-input admin-input-medium" placeholder="Nome do evento" />
+                    <input type="date" className="admin-input admin-input-small" placeholder="Data" />
+                    <input type="text" className="admin-input admin-input-small" placeholder="Hora" />
+                    <input type="number" className="admin-input admin-input-small" placeholder="Valor do evento" />
+                    <input type="text" className="admin-input admin-input-big" placeholder="Classificação do evento" />
+                    <select className="admin-input admin-input-medium">
+                        <option>Selecione uma categoria</option>
+                        <option>A</option>
+                        <option>b</option>
+                    </select>
+
+                    <select className="admin-input admin-input-medium">
+                        <option>Selecione um enderelo</option>
+                        <option>A</option>
+                        <option>b</option>
+                    </select>
+                    <textarea className="admin-input admin-input-big " placeholder="Descrição do evento"></textarea>
+                    <input type="file" className="admin-input admin-input-medium input-file" />
+
+                    <div className="admin-form-button_conatiner">
+                        <Link to="/admin">Cancelar</Link>
+                        <button type="submit">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
