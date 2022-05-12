f5d440878d7767bba71d45a986ad13cc221e527b
Bruno Pontes <bruno_confessor@hotmail.com>
2022-05-12 00:44:22 -0300

    Estruturado pagina HTML

--- frontend/src/Pages/OrderCompleted/OrderCompleted.js
+++ frontend/src/Pages/OrderCompleted/OrderCompleted.js
@@ -1,8 +1,49 @@
 import React from "react";
-
+import "./OrderCompleted.scss";
 function OrderCompletedPage() {
     return (
-        <h1>Order Completed</h1>
+        <div>
+            <div>
+                <div>
+                    <img src="./image/CheckCircle.png" />
+                </div>
+                <div>
+                    <h1>Seu pedido esta em análise</h1>
+                    <span><p>Não se preocupe</p></span>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Nibh tellus molestie nunc non blandit massa enim. Pellentesque</p>
+            </div>
+            <div>
+                <h1>seu Pedido</h1>
+                <div>
+                    <div>
+                        <span><p>Preço Uni.</p></span>
+                        <span><p>Quantid.</p></span>
+                        <span><p>Preço</p></span>
+                    </div>
+                </div>
+                <div>
+                    <image src="#" />
+                    <div>
+                        <p>Show de Mágica Infantil</p>
+                        <p>12/12/2022 - 15:00</p>
+                        <p>Teatro Bibi</p>
+                        <p>Ferreira - SP</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>R$ 0,00</p>
+                            <p>10</p>
+                            <p>R$ 0,00</p>
+                        </div>
+                        <div>
+                            <span><p>Total</p></span>
+                            <span><p>R$ 0,00</p></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
