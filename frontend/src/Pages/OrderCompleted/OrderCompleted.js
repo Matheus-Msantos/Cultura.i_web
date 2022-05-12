a2ca37fbb204960e20cf5eec02a80ea0686efb4a
Bruno Pontes <bruno_confessor@hotmail.com>
2022-05-12 01:50:56 -0300

    Alinhado lado esquedo da pagina com imagem

--- frontend/src/Pages/OrderCompleted/OrderCompleted.js
+++ frontend/src/Pages/OrderCompleted/OrderCompleted.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./OrderCompleted.scss";
 function OrderCompletedPage() {
     return (
-        <div>
-            <div>
-                <div>
-                    <img src="./image/CheckCircle.png" />
+        <div className="div-general-order-completed">
+            <div className="div-img-message">
+                <div className="div-img-circle">
+                    <img src="./image/CheckCircle.png" className="img-circle" />
                 </div>
-                <div>
-                    <h1>Seu pedido esta em análise</h1>
-                    <span><p>Não se preocupe</p></span>
+                <div className="div-title-product">
+                    <h1 className="h1-title">Seu pedido esta em análise</h1>
+                    <span><p className="span-title-message">Não se preocupe</p></span>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Nibh tellus molestie nunc non blandit massa enim. Pellentesque</p>
+                <div className="div-message">
+                    <p className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Nibh tellus molestie nunc non blandit massa enim. Pellentesque</p>
+                </div>
             </div>
-            <div>
-                <h1>seu Pedido</h1>
-                <div>
+            <div className="div-data-info-completed">
+                <h1 className="h1-title-products">Seu Pedido</h1>
+                <div className="div-head-info">
                     <div>
                         <span><p>Preço Uni.</p></span>
                         <span><p>Quantid.</p></span>
