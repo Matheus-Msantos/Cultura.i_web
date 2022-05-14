69797a395f0036048b082b7f5f8bec5e7b4df884
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-05-14 04:37:43 -0300

    Página admin usuário epedidos crianda, junto com as integreções com API

--- frontend/src/Pages/Admin/Users/ListUser.js
+++ frontend/src/Pages/Admin/Users/ListUser.js
@@ -1,10 +1,95 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import MenuAdmin from "../../../components/MenuAdmin";
+import { BaseUrl } from "../../../Api/baseUrl";
+import { UserContext } from "../../../Auth";
+import { useState } from "react";
+
+import OptionBoxAdmin from "../../../components/OptionBoxAdmin/index";
+
 
 function AdminUserPage() {
+
+
+    const [usuarios, setUsuarios] = useState([]);
+    const [box, setBox] = useState(false);
+    /* Contexto do Usuário */
+    const { currentUser } = useContext(UserContext);
+    /* Guardando Token */
+    const token = currentUser?.token;
+    /* Passando Token no header para API */
+    BaseUrl.defaults.headers.authorization = `Bearer ${token}`;
+
+    useEffect(() => {
+        /* Chamada da API de todos os usuarios */
+        BaseUrl
+            .get("/api/user")
+            .then((res) => setUsuarios(res.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+
+    const handleBox = () => {
+        if (box === false)
+            setBox(true);
+        else
+            setBox(false);
+    }
+
+    /* Mapeando todos os usuários e adicionando na página */
+    const MapUsuarios = usuarios.map((usuario) => {
+        const { id, image, name, email, cpf_cnpj, is_Admin, is_Producer, is_Advertiser } = usuario;
+        return (
+            <tr key={id}>
+                <th scope="row">{id}</th>
+                <td>{name}</td>
+                <td>{email}</td>
+                <td>{cpf_cnpj}</td>
+                <td>{is_Admin}</td>
+                <td>{is_Producer}</td>
+                <td>{is_Advertiser}</td>
+                <td>
+                    <img src={image} />
+                </td>
+                <td>
+                    <button onClick={() => handleBox()}><i className="fa-solid fa-ellipsis"></i></button>
+
+                    <div className={`admin-box_container ${box && 'is--active'}`}>
+                        <OptionBoxAdmin url={`/admin/user/edit/${id}`} />
+                    </div>
+                </td>
+            </tr>
+        );
+    });
+
     return (
         <div className="admin-container">
             <MenuAdmin active_05={"is--active"} />
+
+
+            <div className="admin-content_container">
+
+                <h2>Usuários</h2>
+
+                <table className="table align-middle">
+                    <thead >
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">email</th>
+                            <th scope="col">CPF/CNPJ</th>
+                            <th scope="col">Admin</th>
+                            <th scope="col">Produtor</th>
+                            <th scope="col">Anúnciante</th>
+                            <th scope="col">Imagem</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        {MapUsuarios}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
