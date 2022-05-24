3eeab425b09cfd33378e3b9c39cefb41df36e562
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-05-23 22:21:03 -0300

    Fix: URL API

--- frontend/src/Api/baseUrl.js
+++ frontend/src/Api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BaseUrl = axios.create({
-    baseURL: "https://clownfish-app-ntk6p.ondigitalocean.app",
+    baseURL: "https://sea-turtle-app-k9xb5.ondigitalocean.app",
 });
 
 
