9101280caa44c7d8a99b63d52bab0be0afcdbd49
Matheus Melo Santos <66855345+Matheus-Msantos@users.noreply.github.com>
2022-05-24 02:33:02 -0300

    Update baseUrl.js

--- frontend/src/Api/baseUrl.js
+++ frontend/src/Api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BaseUrl = axios.create({
-    baseURL: "https://clownfish-app-jrome.ondigitalocean.app",
+    baseURL: "https://sea-turtle-app-om7jd.ondigitalocean.app",
 });
 
 
