664fe5a1c950c9cfa0b8c091240f4b2a3a251c9b
Matheus Melo Santos <66855345+Matheus-Msantos@users.noreply.github.com>
2022-05-31 22:04:47 -0300

    Update baseUrl.js

--- frontend/src/Api/baseUrl.js
+++ frontend/src/Api/baseUrl.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const BaseUrl = axios.create({
-    //baseURL: "https://sea-turtle-app-om7jd.ondigitalocean.app",
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://sea-turtle-app-om7jd.ondigitalocean.app",
+    //baseURL: "http://127.0.0.1:8000",
 });
 
 
